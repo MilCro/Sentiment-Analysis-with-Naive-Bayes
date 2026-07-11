25e13d330f8f0e7a89a443d4bbbdad03f24c64a4
Millie C <milliecroucher99@gmail.com>
2026-07-11 20:20:43 +0100

    realised what i was doing was a waste of time

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,8 +1,45 @@
 
 import csv
+import nltk
+nltk.download('opinion_lexicon')
+from nltk.corpus import opinion_lexicon
+
+positive_words = opinion_lexicon.positive()
+negative_words = opinion_lexicon.negative()
+
+num_pos = len(positive_words) #2006
+num_neg = len(negative_words) #4783
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+import csv
 import numpy as np
 import re
 import nltk
+nltk.download("averaged_perceptron_tagger_eng") #list of words tagged with types
 from nltk.corpus import stopwords
 nltk.download("stopwords") #list of unimportant words e.g. the
 from nltk.stem import PorterStemmer #reduces words to their root form
@@ -13,14 +50,26 @@ from nltk import pos_tag #tags words with their type e.g. nouns NN
 class Cleaning:
     def __init__(self, review):
         self.content = review[0].lower()
-    
-    #the following methods should be completed in the order written 
+        print(self.content)
+        self.tokenise()
+        print(self.content)
+        self.remove_punctuation()
+        print(self.content)
+        self.remove_stop_words()
+        print(self.content)
+        self.remove_nouns()
+        print(self.content)
+        self.stemming()
+        print(self.content)
 
-    def remove_punctuation(self):
-        self.content = re.sub(r'[^a-zA-Z]', '', self.content)
+    #the following methods should be completed in the order written 
 
     def tokenise(self):
         self.content = self.content.split()
+
+    def remove_punctuation(self):
+        for word in self.content:
+            word = re.sub(r'[^a-zA-Z]', '', word)
 
     def remove_stop_words(self):
         self.content = [word for word in self.content if word not in stop_words_arr]
@@ -33,27 +82,31 @@ class Cleaning:
         stemmer = PorterStemmer()
         self.content = [stemmer.stem(word) for word in self.content]
 
+    def get_review(self):
+        return self.content
+
 
 stop_words_arr = stopwords.words("english")
-
-cleaner = Cleaning()
 
 with open("data/movie.csv","r",encoding="utf-8-sig") as data:
     reader = csv.reader(data)
     writer = csv.writer(data, delimiter=",")
     counter = 0
     for row in reader:
-        cleaned_review = cleaner(row)
+        cleaning = Cleaning(row)
+        cleaned_review = cleaning.get_review()
+        print("\ncleaned review:", cleaned_review)
         #if counter <= 20000:
         if counter <= 3:
             with open("data/training_data.csv") as training_data:
-                writer.writerow(cleaned_review,row[1])
+                writer.writerow(cleaned_review)
+                writer.writerow(row[1])
         #else:
         elif counter <= 6:
             with open("data/testing_data.csv") as testing_data:
                 writer.writerow(cleaned_review,row[1])
         counter += 1
-
+"""
 
 
 
