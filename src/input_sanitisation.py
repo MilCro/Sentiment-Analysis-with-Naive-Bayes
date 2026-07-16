980a9d79b5551b365b78c5418ebd40e33d6189f6
Millie C <milliecroucher99@gmail.com>
2026-07-16 16:02:50 +0100

    Makes input usable

--- src/input_sanitisation.py
+++ src/input_sanitisation.py
@@ -10,6 +10,7 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer #reduces words to their root form
 #nltk.download("punkt")
 #nltk.download("wordnet")
+from nltk.corpus import opinion_lexicon
 
 """
 Things to do to inputs before analysis:
@@ -43,9 +44,36 @@ class Sanitisation:
 
 stop_words_arr = stopwords.words("english")
 
+positive_words = opinion_lexicon.positive()
+negative_words = opinion_lexicon.negative()
+vocabulary = positive_words + negative_words #6789
+
 class Preparation:
     def __init__(self,input):
-        self.input = self.input
-        self.length = len(self.input)
-        self.output = np.zeros(self.length)
+        self.input = input
+        self.output = np.zeros(6789)
+
+    def get_output(self):
+        return self.output
+
+    def prepare(self):
+        col = 0
+        for word in vocabulary:
+            if word in self.input:
+                self.output[col] = 1
+            col += 1
+
+
+
+
+
+
+input = "A polar bear's head is oblong and relatively small compared to body size. The muzzle is elongated with a 'Roman-nosed' (slightly arched) snout. Polar bears have 42 teeth, which they use for catching food and for aggressive behavior. Polar bears use their incisors to shear off pieces of blubber and flesh."
+
+sanitiser = Sanitisation(input)
+print(sanitiser.get_input())
+clean_input = sanitiser.get_input()
+preparing = Preparation(clean_input)
+print(preparing.get_output())
+ready_to_use_input = preparing.get_output()
 
