6ba016e8d678289c803fc83c7d3547fec51d908c
Millie C <milliecroucher99@gmail.com>
2026-08-01 18:10:11 +0100

    Estimating log class priors

--- src/analysis.py
+++ src/analysis.py
@@ -10,11 +10,20 @@ class Analysis:
     def __init__(self, k, training_data):
         self.k = k
         self.log_class_priors = np.array([0.0,0.0])
-        self.log_class_conditional_likelihoods = np.zeros(shape=(2,55)) #will be a lot more than 55
+        self.log_class_conditional_likelihoods = np.zeros(shape=(2,6790)) 
         self.training_data = training_data
 
-    def estimate_log_class_priors(self,data):
-        pass
+    def estimate_log_class_priors(self, data):
+        numPos = 0
+        numNeg = 0
+        for review in data:
+            if review[0] == 1:
+                numPos += 1
+            else:
+                numNeg += 1
+        logSpam = np.log10(numPos/55) #eventually 55 will be 20,000
+        logHam = np.log10(numNeg/55)
+        self.log_class_priors = np.array([logHam,logSpam])
 
     def estimate_log_class_conditional_likelihoods(self,data):
         pass
